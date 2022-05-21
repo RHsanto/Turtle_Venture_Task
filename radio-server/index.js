964caea15b47aa2c86c85276e097ad206249866c
RHsanto <rakibulsanto789@gmail.com>
2022-05-21 23:35:26 +0600

    [add] api

--- radio-server/index.js
+++ radio-server/index.js
@@ -18,6 +18,7 @@ async function run() {
     await client.connect();
     const database = client.db("allDataCollection");
     const UserCollection = database.collection("userCollection");
+    const RadioCollection = database.collection("radioCollection");
     
    
 
@@ -28,26 +29,60 @@ async function run() {
   res.send(bus);
 
  });
+ // GET API REVIEWS
+ app.get('/all-station', async (req,res)=>{
+  const cursor = RadioCollection.find({});
+  const bus = await cursor.toArray();
+  res.send(bus);
+
+ });
 
  
- 
   // GET SINGLE OFFERS
-  app.get('/users/:id', async (req,res)=>{
+  app.get('/all-station/:id', async (req,res)=>{
     const id = req.params.id;
     const query = {_id: ObjectId(id)};
-    const booking = await UserCollection.findOne(query)
+    const booking = await RadioCollection.findOne(query)
   res.json(booking);
   });
   
   // here put booking data
-  app.post('/user', async (req,res) => {
+  app.post('/users', async (req,res) => {
     const user = req.body;
     const result = await UserCollection.insertOne(user);
     res.json( result)
   })
+  // here post station data
+  app.post('/all-station', async (req,res) => {
+    const user = req.body;
+    const result = await RadioCollection.insertOne(user);
+    res.json( result)
+  })
 
+ //DELETE API 
+ app.delete('/all-station/:id', async(req,res)=>{
+  const id     = req.params.id;
+  const query  = {_id:ObjectId(id)} ;
+  const result = await RadioCollection.deleteOne(query)
+  console.log(id,result);
+  res.json(result);
+ })
 
-
+   // UPDATE STATUS 
+   app.put('/all-station/:id', async(req,res)=>{
+    const body =req.body;
+    const id = req.params.id;
+    const filter ={_id: ObjectId(id)}
+    const option = {upsert : true};
+    const updateStatus ={
+      $set:{
+       name:body.name,
+       radio:body.radio
+      },
+    };
+    const result = await RadioCollection.updateOne(filter,updateStatus,option);
+    res.json(result)
+  })
 
   } 
   
