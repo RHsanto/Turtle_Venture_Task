ab1fa26e344d4996e83f65161a933d804e769149
RHsanto <rakibulsanto789@gmail.com>
2022-05-22 11:53:35 +0600

    [add]

--- task-radio/src/App.js
+++ task-radio/src/App.js
@@ -1,5 +1,4 @@
 
-import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './page/common/Navbar';
@@ -8,22 +7,10 @@ import SignIN from './page/Task_4_API_Server/Authentication/SignIN';
 import SignUp from './page/Task_4_API_Server/Authentication/SignUp';
 import AddStation from './page/Task_4_API_Server/Station/AddStation';
 import AllStation from './page/Task_4_API_Server/Station/AllStation';
-import { gapi } from 'gapi-script';
-// here use client id
-const clientId="997390170447-rkpk3l9lltfi7d9qbtcukksodou5sc75.apps.googleusercontent.com"
+
 
 function App() {
-  // here use function for oauth2.0
- useEffect(()=>{
-   function start(){
-     gapi.client.init({
-      clientId:clientId,
-      scope:""
-     })
-   };
-   gapi.load("client:auth2",start)
- })
-  
+
   return (
     <div className="App">
        <BrowserRouter>
--- task-radio/src/page/common/Navbar.js
+++ task-radio/src/page/common/Navbar.js
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-const clientId ="997390170447-rkpk3l9lltfi7d9qbtcukksodou5sc75.apps.googleusercontent.com"
+import useFirebase from '../../hooks/useFirebase';
+import { FaUserCircle } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
+
+
 const Navbar = () => {
-  // const { user ,logOut} = useFirebase();
-  const [user,setUser]=useState("")
-  const onSuccess=(response)=>{
-    setUser(response.profileObj);
-  }
-  const onFailure=(response)=>{
-    console.log("LOGIN FAIELD! Current user :",response);
-  }
-  const logout=()=>{
-   alert(" Log out Confirm!")
-   window.location.reload()
-  }
+  const { user,logOut} = useFirebase();
+
   return (
     <div className='menubar'>
     <div className="container">
@@ -26,7 +19,7 @@ const Navbar = () => {
        <NavLink to='/'>Radio Widget</NavLink>
        <NavLink to='/all-station'>All Station</NavLink>
        <NavLink to='/add-station'>Add Station</NavLink>
-       {/* <>
+       <>
         {user?.email ?
     <>  {user.photoURL ?
     <img className="UserImg ms-4" src={user.photoURL} alt="" />
@@ -39,31 +32,9 @@ const Navbar = () => {
     <><NavLink to='/sign-in'><button className='ms-2 bg-primary border-0 text-light  p-2 rounded' >
     Sign-in</button></NavLink></>
   }
-        </> */}
-       
-      {user?.email ? 
-      <div className='d-flex align-items-center'>
-          <div className='ms-4 me-2'>{user?.givenName}</div>
-      <div ><img  src={user?.imageUrl} alt="" className="UserImg me-4"/></div>  <GoogleLogout
-      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-      buttonText="Logout"
-      onLogoutSuccess={logout}
-    /></div> 
-      : <div className='ms-4'>
-      <GoogleLogin
-  clientId={clientId}
-  buttonText="Login"
-  onSuccess={onSuccess}
-  onFailure={onFailure}
-  cookiePolicy={'single_host_origin'}
-/></div>
-    }
+        </>
        <div>
-      
-    
        </div>
-
-
        </div>
      </div>
         </div>
